65bb956e9cdbb1437b74f5cd9e9b6e44c79a0e00
hong yuseok <jericoh@naver.com>
2021-06-19 15:14:14 +0900

    chapter02 complete

--- readme.md
+++ readme.md
@@ -6,3 +6,7 @@
  * 인텔리제이에서 깃과 깃허브 사용하기
 
 ## Spring Boot Chapter 02
+ * TDD와 단위 테스트
+ * 테스트 코드작성    
+   간단한 controller를 작성하고 그에 따른 테트트 코드 작성하기
+ * 롬복(Getter, Setter, 기본생성자, toString 등을 자동 생성) 사용 방법  
--- src/main/java/com/spring_boot/springboot/web/HelloController.java
+++ src/main/java/com/spring_boot/springboot/web/HelloController.java
@@ -1,13 +1,20 @@
 package com.spring_boot.springboot.web;
 
+import com.spring_boot.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // json을 반환하는 컨트롤러를 만들어 줍니다
-
 public class HelloController {
     @GetMapping("/hello") // Get 요청을 받을 수 있는 API를 만들어 줍니다, '/hello'로 요청이 드렁오면 "hello"라는 문자열 반환
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
